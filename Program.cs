f933284420d1fd5f4879c8b2a87f3c2e76a3bf5a
Kamil Kondziolka <wono83@interia.pl>
2024-12-07 17:01:43 +0100

    Added printing both answers at the same time

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using var inputReader = new StreamReader(Path.Join(Directory.GetCurrentDirectory(), "input.txt"));
 int[] members;
-long answer = 0;
+long part1Answer = 0, part2Answer = 0;
 while (!inputReader.EndOfStream)
 {
     var line = await inputReader.ReadLineAsync();
@@ -8,17 +8,26 @@ while (!inputReader.EndOfStream)
     {
         var result = long.Parse(line.Split(':')[0]);
         members = line.Split(':')[1].Split(' ').Skip(1).Select(int.Parse).ToArray(); //Skip 1 to skip first empty string
-        answer = IsResultPossible(result, members[0]) ? answer + result : answer;
+        part1Answer = IsResultPossible(result, members[0]) ? part1Answer + result : part1Answer;
+        part2Answer = IsResultPossibleWithConc(result, members[0]) ? part2Answer + result : part2Answer;
     }
 }
 
-Console.WriteLine(answer);
+Console.WriteLine($"First part: {part1Answer}");
+Console.WriteLine($"Second part: {part2Answer}");
 return;
 
 bool IsResultPossible(long expectedResult, long previousResult, int pointer = 1)
 {
     if (pointer >= members.Length) return previousResult == expectedResult;
     return IsResultPossible(expectedResult, previousResult * members[pointer], pointer + 1) ||
-           IsResultPossible(expectedResult, previousResult + members[pointer], pointer + 1) ||
-           IsResultPossible(expectedResult, long.Parse(previousResult.ToString() + members[pointer]), pointer + 1);
+           IsResultPossible(expectedResult, previousResult + members[pointer], pointer + 1);
+}
+
+bool IsResultPossibleWithConc(long expectedResult, long previousResult, int pointer = 1)
+{
+    if (pointer >= members.Length) return previousResult == expectedResult;
+    return IsResultPossibleWithConc(expectedResult, previousResult * members[pointer], pointer + 1) ||
+           IsResultPossibleWithConc(expectedResult, previousResult + members[pointer], pointer + 1) ||
+           IsResultPossibleWithConc(expectedResult, long.Parse(previousResult.ToString() + members[pointer]), pointer + 1);
 }

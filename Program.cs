e72ca4d8f86dddeb4c34eb788ab624ed6ee1bb46
Kamil Kondziolka <wono83@interia.pl>
2024-12-07 16:50:08 +0100

    Advent of code day 7

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using var inputReader = new StreamReader(Path.Join(Directory.GetCurrentDirectory(), "input.txt"));
+int[] members;
+long answer = 0;
+while (!inputReader.EndOfStream)
+{
+    var line = await inputReader.ReadLineAsync();
+    if (line != null)
+    {
+        var result = long.Parse(line.Split(':')[0]);
+        members = line.Split(':')[1].Split(' ').Skip(1).Select(int.Parse).ToArray(); //Skip 1 to skip first empty string
+        answer = IsResultPossible(result, members[0]) ? answer + result : answer;
+    }
+}
+
+Console.WriteLine(answer);
+return;
+
+bool IsResultPossible(long expectedResult, long previousResult, int pointer = 1)
+{
+    if (pointer >= members.Length) return previousResult == expectedResult;
+    return IsResultPossible(expectedResult, previousResult * members[pointer], pointer + 1) ||
+           IsResultPossible(expectedResult, previousResult + members[pointer], pointer + 1) ||
+           IsResultPossible(expectedResult, long.Parse(previousResult.ToString() + members[pointer]), pointer + 1);
+}
